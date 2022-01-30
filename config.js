93155a007746eb2b4cadc05cbb7443cc8b0b96a9
Sagar Tamang <mi5t4n@gmail.com>
2022-01-30 14:04:33 +0545

    Added disqus comment system.

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
   title: 'Sagar Tamang',
   subtitle: 'Senior WordPress Plugin Developer',
   copyright: '© All rights reserved.',
-  disqusShortname: '',
+  disqusShortname: 'sagartamang',
   postsPerPage: 4,
   googleAnalyticsId: 'UA-73379983-2',
   useKatex: false,
